664014afb802da1cfc5ac49796555682cf3f9efb
marcocab <marco.caballero.d@gmail.com>
2019-05-01 18:07:37 +0200

    HOT-FIX: Tableau isCardAdditionAllowed.

--- src/app/model/tableau.ts
+++ src/app/model/tableau.ts
@@ -8,7 +8,7 @@ export class Tableau {
     }
 
     isCardAdditionAllowed(card: Card): boolean {
-        return (this.tail().isRed()) ? !card.isRed(): card.isRed();
+        return (this.head().isRed()) ? !card.isRed(): card.isRed();
     }
 
     addCard(card: Card): void {
@@ -27,11 +27,11 @@ export class Tableau {
         return this._cards;
     }
 
-    private head(): Card {
+    head(): Card {
         return this._cards[this._cards.length - 1];
     }
 
-    private tail(): Card {
+    tail(): Card {
         return this._cards[0];
     }
 }

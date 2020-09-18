7ef9f635b603afaaa386cb9c1413eb4327b00f54
marcocab <marco.caballero.d@gmail.com>
2020-09-18 23:18:49 +0200

    Adding bash script to deploy from travis.

--- build.sh
+++ build.sh
@@ -3,9 +3,12 @@
 DEPLOYMENT_NAME="klondike-deployment"
 NAMESPACE="klondike-ns"
 IMAGE_REPOSITORY="badibadi"
+RELEASE_REPOSITORY="marcocab"
 IMAGE_NAME="klondike-app"
 APP_SERVICE="klondike-app-service"
 IMAGE_TAG="0.0.1"
+DEV="dev"
+LATEST="latest"
 
 _help() {
 cat << EOF
@@ -64,6 +67,25 @@ _build() {
     echo "Output: ${output}"
 }
 
+_release_ci() {
+    # Private travis env variables $DOCKER_USERNAME, $DOCKER_PASSWORD
+    echo "RELEASE CI\n"
+    echo "Build image $RELEASE_REPOSITORY/$IMAGE_NAME:$DEV ...\n" 
+    docker build -t $RELEASE_REPOSITORY/$IMAGE_NAME:$DEV .
+
+    echo "Tag image $RELEASE_REPOSITORY/$IMAGE_NAME:$DEV --> $RELEASE_REPOSITORY/$IMAGE_NAME:$LATEST...\n" 
+    docker tag $RELEASE_REPOSITORY/$IMAGE_NAME:$DEV $RELEASE_REPOSITORY/$IMAGE_NAME:$LATEST
+
+    echo "Login into docker hub. ...\n" 
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin 
+    
+    echo "Push $RELEASE_REPOSITORY/$IMAGE_NAME:$DEV...\n" 
+    docker push $RELEASE_REPOSITORY/$IMAGE_NAME:$DEV
+
+    echo "Push $RELEASE_REPOSITORY/$IMAGE_NAME:$LATEST...\n" 
+    docker push $RELEASE_REPOSITORY/$IMAGE_NAME:$LATEST
+}
+
 [ $# -eq 0 ] && echo "No args, no party. Try ${0##*/} -h" && exit 0
 
 while [ $# -ne 0 ]; do
@@ -86,6 +108,10 @@ while [ $# -ne 0 ]; do
             ;;
         -c|--clean)
             _clean
+            shift
+            ;;
+        -rci|--release-ci)
+            _release_ci
             shift
             ;;
         ?*)
